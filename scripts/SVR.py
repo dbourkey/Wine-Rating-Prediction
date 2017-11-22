b173eb1df01d76d040f9d0fd61c15b8c7a83ef8d
David Bourke <davidbourke94@gmail.com>
2017-11-22 21:04:47 +0000

    Added comments to generator

--- scripts/SVR.py
+++ scripts/SVR.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+# Assumed input is a CSV file of numeric data
 def instanceGenerator(num_instances, file_loc, y_index, seperator=";", skip_header=True):
     with open(file_loc, "r") as input_file:
         if skip_header:
             input_file.readline()
         for i in range(num_instances):
+            # TODO: deal with cases where y_index is in middle of list, multiple outputs
             instance_data_raw = input_file.readline().split(seperator)
             X = np.array(instance_data_raw[:y_index])
             y = instance_data_raw[y_index]
