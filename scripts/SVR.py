59aec9788bffd0d365e557ee88ecf3acb681b561
David Bourke <davidbourke94@gmail.com>
2017-11-23 15:10:04 +0000

    Calculated R2 score

--- scripts/SVR.py
+++ scripts/SVR.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-from sklearn import svm
+from sklearn.svm import SVR
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import normalize
+from sklearn.preprocessing import StandardScaler
 
 # Assumed input is a CSV file of numeric data
 def instanceGenerator(num_instances, file_loc, seperator=";", skip_instances = 0, skip_header=True):
@@ -13,14 +16,8 @@ def instanceGenerator(num_instances, file_loc, seperator=";", skip_instances = 0
 
             yield np.array(instance_data_raw, dtype=np.float64)
 
-train = 8
-test = 2
-"""
-training_data = [(X_instance, y_instance) for X_instance, y_instance in 
-    instanceGenerator(train, "../WineData/winequality-red.csv", 11)][0]
-testing_data = [(X_instance, y_instance) for X_instance, y_instance in 
-    instanceGenerator(test, "../WineData/winequality-red.csv", 11, skip_instances=train)]
-"""
+train = 1500
+test = 400
 
 training_data = np.array([instance for instance in instanceGenerator(train, "../WineData/winequality-red.csv")])
 testing_data = np.array([instance for instance in instanceGenerator(test, "../WineData/winequality-red.csv", skip_instances=train)])
@@ -28,5 +25,16 @@ testing_data = np.array([instance for instance in instanceGenerator(test, "../Wi
 X_train = np.array([training_data[i][:11] for i in range(len(training_data))])
 y_train = np.array([training_data[i][11] for i in range(len(training_data))])
 
-X_train = np.array([training_data[i][:11] for i in range(len(testing_data))])
-y_train = np.array([training_data[i][11] for i in range(len(testing_data))])
+X_test = np.array([training_data[i][:11] for i in range(len(testing_data))])
+y_test = np.array([training_data[i][11] for i in range(len(testing_data))])
+
+scaler = StandardScaler().fit(X_train)
+
+
+clf = SVR(kernel="rbf", C=1.0, epsilon=0.2)
+
+clf.fit(X_train, y_train)
+
+pred = clf.predict(X_test)
+
+print("R2 score: ", r2_score(y_test, pred))
