99391272181b7c4cc411c0b01be2a264cad03a65
David Bourke <davidbourke94@gmail.com>
2017-11-27 18:50:27 +0000

    More tests

--- scripts/SVR.py
+++ scripts/SVR.py
@@ -8,6 +8,7 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_squared_log_error
 from sklearn.metrics import median_absolute_error
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import normalize
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -31,10 +32,14 @@ data_size = 4898
 data = np.array([instance for instance in instanceGenerator(data_size, "../WineData/winequality-white.csv")])
 
 #X_data = np.array([np.array([data[i][10], data[i][5], data[i][2]]) for i in range(len(data))])
-X_data = np.array([data[i][:11] for i in range(len(data))])
+X_data = np.array([
+    np.array([data[i][10], data[i][7], data[i][2], data[i][0], data[i][8]]) 
+    for i in range(len(data))])
 y_data = np.array([data[i][11] for i in range(len(data))])
 
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.3, random_state=6)
+
+y_rand = np.random.randint(11, size=len(y_test))
 
 #X_train = np.array([training_data[i][:11] for i in range(len(training_data))])
 #y_train = np.array([training_data[i][11] for i in range(len(training_data))])
@@ -59,6 +64,10 @@ print("Median Absolute Error:\t", median_absolute_error(y_test, pred))
 pred_int = np.array([round(y) for y in pred], dtype=np.int32)
 y_test_int = np.array([round(y) for y in y_test], dtype=np.int32)
 
+for i in range(len(pred_int)):
+    if (abs(y_test_int[i] - pred_int[i]) > 2):
+        print(y_test_int[i], pred_int[i])
+
 print("With int output")
 print("R2 score:\t\t", r2_score(y_test_int, pred_int))
 print("Explained Var:\t\t", explained_variance_score(y_test_int, pred_int))
@@ -66,3 +75,14 @@ print("Mean Absolute Error:\t", mean_absolute_error(y_test_int, pred_int))
 print("Mean Squared Error:\t", mean_squared_error(y_test_int, pred_int))
 print("Mean Squared Log Error:\t", mean_squared_log_error(y_test_int, pred_int))
 print("Median Absolute Error:\t", median_absolute_error(y_test_int, pred_int))
+print("Accuracy:\t", accuracy_score(y_test_int, pred_int))
+
+print("Comparison with random data")
+print("With int output")
+print("R2 score:\t\t", r2_score(y_rand, pred_int))
+print("Explained Var:\t\t", explained_variance_score(y_rand, pred_int))
+print("Mean Absolute Error:\t", mean_absolute_error(y_rand, pred_int))
+print("Mean Squared Error:\t", mean_squared_error(y_rand, pred_int))
+print("Mean Squared Log Error:\t", mean_squared_log_error(y_rand, pred_int))
+print("Median Absolute Error:\t", median_absolute_error(y_rand, pred_int))
+print("Accuracy:\t", accuracy_score(y_rand, pred_int))
