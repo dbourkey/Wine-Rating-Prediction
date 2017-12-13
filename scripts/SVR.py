51e57a1e2a22bb005934570b4b5418f66000c533
David Bourke <davidbourke94@gmail.com>
2017-12-13 16:35:19 +0000

    added confusion matrix

--- scripts/SVR.py
+++ scripts/SVR.py
@@ -9,9 +9,12 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_squared_log_error
 from sklearn.metrics import median_absolute_error
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import normalize
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
 
 
 # Assumed input is a CSV file of numeric data
@@ -27,14 +30,15 @@ def instanceGenerator(num_instances, file_loc, seperator=";", skip_instances = 0
 
             yield np.array(instance_data_raw, dtype=np.float64)
 
-data_size = 4898
 
-data = np.array([instance for instance in instanceGenerator(data_size, "../WineData/winequality-white.csv")])
+data_size = 1000
 
-#X_data = np.array([np.array([data[i][10], data[i][5], data[i][2]]) for i in range(len(data))])
-X_data = np.array([
-    np.array([data[i][10], data[i][7], data[i][2], data[i][0], data[i][8]]) 
-    for i in range(len(data))])
+data = np.array([instance for instance in instanceGenerator(data_size, "../WineData/winequality-red.csv")])
+
+X_data = np.array([data[i][:11] for i in range(len(data))])
+#X_data = np.array([
+#    np.array([data[i][10], data[i][7], data[i][2], data[i][0], data[i][8]]) 
+#    for i in range(len(data))])
 y_data = np.array([data[i][11] for i in range(len(data))])
 
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.3, random_state=6)
@@ -64,9 +68,10 @@ print("Median Absolute Error:\t", median_absolute_error(y_test, pred))
 pred_int = np.array([round(y) for y in pred], dtype=np.int32)
 y_test_int = np.array([round(y) for y in y_test], dtype=np.int32)
 
-for i in range(len(pred_int)):
-    if (abs(y_test_int[i] - pred_int[i]) > 2):
-        print(y_test_int[i], pred_int[i])
+plt.hist(pred_int, alpha=0.5, bins=10)
+plt.hist(y_test, alpha=0.5, bins=10)
+plt.title("Predicted Quality")
+plt.show()
 
 print("With int output")
 print("R2 score:\t\t", r2_score(y_test_int, pred_int))
@@ -77,12 +82,26 @@ print("Mean Squared Log Error:\t", mean_squared_log_error(y_test_int, pred_int))
 print("Median Absolute Error:\t", median_absolute_error(y_test_int, pred_int))
 print("Accuracy:\t", accuracy_score(y_test_int, pred_int))
 
+print(confusion_matrix(y_test_int, pred_int))
+
+"""
 print("Comparison with random data")
-print("With int output")
-print("R2 score:\t\t", r2_score(y_rand, pred_int))
-print("Explained Var:\t\t", explained_variance_score(y_rand, pred_int))
-print("Mean Absolute Error:\t", mean_absolute_error(y_rand, pred_int))
-print("Mean Squared Error:\t", mean_squared_error(y_rand, pred_int))
-print("Mean Squared Log Error:\t", mean_squared_log_error(y_rand, pred_int))
-print("Median Absolute Error:\t", median_absolute_error(y_rand, pred_int))
-print("Accuracy:\t", accuracy_score(y_rand, pred_int))
+print("R2 score:\t\t", r2_score(y_test, y_rand))
+print("Explained Var:\t\t", explained_variance_score(y_test, y_rand))
+print("Mean Absolute Error:\t", mean_absolute_error(y_test, y_rand))
+print("Mean Squared Error:\t", mean_squared_error(y_test, y_rand))
+print("Mean Squared Log Error:\t", mean_squared_log_error(y_test, y_rand))
+print("Median Absolute Error:\t", median_absolute_error(y_test, y_rand))
+print("Accuracy:\t", accuracy_score(y_test, y_rand))
+
+mean_score = np.mean(y_train)
+five = np.array([mean_score for x in range(len(y_test))])
+
+print("Compare with mean")
+print("R2 score:\t\t", r2_score(y_test, five))
+print("Explained Var:\t\t", explained_variance_score(y_test, five))
+print("Mean Absolute Error:\t", mean_absolute_error(y_test, five))
+print("Mean Squared Error:\t", mean_squared_error(y_test, five))
+print("Mean Squared Log Error:\t", mean_squared_log_error(y_test, five))
+print("Median Absolute Error:\t", median_absolute_error(y_test, five))
+"""
